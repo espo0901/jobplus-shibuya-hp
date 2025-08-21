c23200d46bd0f1c9db46a00291e5f42a13e502d1
Leon.C <kindlyhearted666@gmail.com>
2025-08-21 14:38:02 +0900

    フッター修正

--- jobplus-shibuya-originaltheme/footer.php
+++ jobplus-shibuya-originaltheme/footer.php
@@ -1,46 +1,50 @@
-<footer class="l-footer">
-  <div class="c-to-top-btn">PAGETOP</div>
+    <footer class="l-footer">
+      <div class="c-to-top-btn">PAGETOP</div>
 
-  <div class="c-bg-decoration">
-    <img src="<?= esc_url(get_theme_file_uri('/img/アセット 13topページ下装飾.png')); ?>" alt="背景装飾">
-  </div>
-
-  <p class="c-footer-message">ご相談、ご質問あればお気軽にお問い合わせください。</p>
-
-  <div class="l-footer__content">
-    <div class="l-footer__info">
-      <div class="c-footer-logo">
-        <img src="<?= esc_url(get_theme_file_uri('./img/site-logo.png')); ?>" alt="">
-        <span>ジョブぷらす渋谷</span>
+      <div class="c-bg-decoration">
+        <img src="<?= esc_url(get_theme_file_uri('/img/アセット 13topページ下装飾.png')); ?>" alt="背景装飾">
       </div>
 
-      <dl class="c-company-info">
-        <dt>会社名</dt>
-        <dd>株式会社シャンカラ</dd>
-        <dt>事業所名</dt>
-        <dd>ジョブぷらす渋谷</dd>
-        <dt>住所</dt>
-        <dd>東京都渋谷区初台1-49-1<br>第30田中ビル4階</dd>
-        <dt>電話番号</dt>
-        <dd>03-6276-1312</dd>
-        <dt>メールアドレス</dt>
-        <dd>a.jobplusshibuya@gmail.com</dd>
-        <dt>事業所番号</dt>
-        <dd>1311301178(A型)<br>1311301178(定着)</dd>
-      </dl>
-    </div>
+      <p class="c-footer-message">ご相談、ご質問あればお気軽にお問い合わせください。</p>
 
-    <div class="l-footer__map">
-      <img src="<?= esc_url(get_theme_file_uri('/img/地図.png')); ?>" alt="手書き地図">
-    </div>
-  </div>
+      <div class="l-footer__content">
+        <div class="l-footer__info">
+          <div class="c-footer-logo">
+            <img src="<?= esc_url(get_theme_file_uri('./img/site-logo.png')); ?>" alt="">
+            <span>ジョブぷらす渋谷</span>
+          </div>
 
-  <ul class="c-footer-nav">
-    <li><a href="#">ホーム</a></li>
-    <li><a href="#">ジョブぷらす渋谷について</a></li>
-    <li><a href="#">会社概要</a></li>
-    <li><a href="#">アクセス・お問い合わせ</a></li>
-  </ul>
+          <dl class="c-company-info">
+            <dt>会社名</dt>
+            <dd>株式会社シャンカラ</dd>
+            <dt>事業所名</dt>
+            <dd>ジョブぷらす渋谷</dd>
+            <dt>住所</dt>
+            <dd>東京都渋谷区初台1-49-1<br>第30田中ビル4階</dd>
+            <dt>電話番号</dt>
+            <dd>03-6276-1312</dd>
+            <dt>メールアドレス</dt>
+            <dd>a.jobplusshibuya@gmail.com</dd>
+            <dt>事業所番号</dt>
+            <dd>1311301178(A型)<br>1311301178(定着)</dd>
+          </dl>
+        </div>
 
-  <p class="c-footer-copy">Copyright &copy; <?= date('Y'); ?> ジョブぷらす渋谷</p>
-</footer>
+        <div class="l-footer__map">
+          <img src="<?= esc_url(get_theme_file_uri('/img/地図.png')); ?>" alt="手書き地図">
+        </div>
+      </div>
+
+      <ul class="c-footer-nav">
+        <li><a href="#">ホーム</a></li>
+        <li><a href="#">ジョブぷらす渋谷について</a></li>
+        <li><a href="#">会社概要</a></li>
+        <li><a href="#">アクセス・お問い合わせ</a></li>
+      </ul>
+
+      <p class="c-footer-copy">Copyright &copy; <?= date('Y'); ?> ジョブぷらす渋谷</p>
+    </footer>
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
